660202b9efe39cf553ed35307d5082c685fab107
Wei Lisi <tlsirius@163.com>
2017-08-15 23:34:03 +0800

    Updated v1.0
    Add tracking class and tidy up the code.

--- tracking.h
+++ tracking.h
@@ -15,29 +15,48 @@
 using namespace cv;
 using namespace std;
 
-#define UNKNOWN_FLOW_THRESH 1e9  
+#define MIN_TARGET_SIZE	250
+#define MAX_TARGET_NUM 10
 
+#define REPLY_OK 0
+#define REPLY_ERR 1
+
+#define DISPLAY_RESULT true;
+
+#define FILE_NAME "img\\%04d.jpg"
 
 class Tracking //Class for the tracking procedure
 {
 private:
-	Target m_target;
+	Target target[10];	// Tracking targets
+	Target tempTarget[10]; // Detected targets in current frame
+	int targetNum ;  // Number of tracking targets 
+	int tempTargetNum ; // Number of detected targets in current frame
+	CvCapture* m_pSrcCapture; 
+	CvVideoWriter* m_pWriterTracking;
+	Mat m_mSrcFrame3;
+	Mat m_mSrcFrame;
+	Mat m_mDiffGaussian;
+	Mat m_mThreshold;
+	int nFrame;
 public:
 	Tracking();
-	~Tracking() {};
+	~Tracking() ;
 	int DisplayHelp();
-	int LoadSequence();
+	int DisplayResult(int nTime);
+	int GetFrameNum();
+
+	int CreateResultVideo();
+	int WriteResultVideo();
+	int ReleaseResultVideo();
+
+	int LoadSequence(char* pcFileName);
+	int LoadNextFrame();
+
 	int TrainObject(CvSVM *svm);
-	/*
-	void makecolorwheel(vector<Scalar> &colorwheel); // Convert the vector to corrsponding color.
-	void motionToColor(Mat flow, Mat &color); // Plot optical flow result as colored image.
-	double gaussrand(); // Generate random number with Gaussian distribution
-	void calcIntensityHist(Mat flowBlock, int *hist0); // Calculate the histogram of intensity in the given block.
-	void train(Mat flow, Mat origin, Rect r, char *fileName); // Train the wake feature using SVM.
-	void predict(Mat flow, Mat origin, Rect r, CvSVM* SVM, Mat sub); // Detect the wakes in the given region using SVM.
-	CvPoint findCenter(const IplImage * Image, CvSeq* contour); // Find the center point of the given connected domain.
-	CvRect generateGroundRegion(Mat mGroundSky); // Generate the ground region
-	*/
+	int ProcessImage();
+	int DetectTarget();
+	int TrackTarget();
 };
 
 
